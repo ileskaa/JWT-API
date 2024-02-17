4857c47dccca1acb6052ffbdd54f7479b4721ab3
ileska <akseli.ingervo@gmail.com>
2024-02-17 19:00:32 +0200

    add comment

--- dateconversion.py
+++ dateconversion.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-# Change this variable to the desired date
+# Change the following variable to the desired date
 date_string = '2024-12-13'
 
 # a datetime object contains all the information from a date object and a time object
